bc6ef0369881a242201e74b783dc617dfb7393d5
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-01-06 15:08:12 -0800

    audio testing

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -15,13 +15,13 @@ class Profile < ApplicationRecord
 	has_attached_file :image, styles: { large: "500x500", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :audioclip 
+  has_attached_file :audioclip, { validate_media_type: false } 
   #validates_attachment_content_type :audioclip, content_type: ['audio/mp3','audio/mpeg']
 
-  has_attached_file :audioclip2 
+  has_attached_file :audioclip2, { validate_media_type: false } 
   #validates_attachment_content_type :audioclip2, content_type: ['audio/mp3','audio/mpeg']
 
-  has_attached_file :audioclip3 
+  has_attached_file :audioclip3, { validate_media_type: false } 
   #validates_attachment_content_type :audioclip3, content_type: ['audio/mp3','audio/mpeg']
 
 
